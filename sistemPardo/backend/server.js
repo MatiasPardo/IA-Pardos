eadfea36b3b3ee770cedc75b974964e4e918748d
Matias Pardo <pardmatias@frba.utn.edu.ar>
2025-05-28 01:47:52 -0300

    Primera version funcionando

--- sistemPardo/backend/server.js
+++ sistemPardo/backend/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 const ollamaUrl = process.env.OLLAMA_URL || 'http://ollama:11434';
 
 // Only allow frontend origin
-const allowedOrigins = ['http://localhost', 'http://127.0.0.1', 'http://<EC2_PUBLIC_IP>', 'https://<your-domain>'];
+const allowedOrigins = ['http://localhost', 'http://127.0.0.1', 'http://ec2-3-145-106-227.us-east-2.compute.amazonaws.com', 'https://pardos.com.ar', 'https://www.pardos.com.ar', 'http://3.145.106.227'];
 
 app.use(cors({
     origin: function (origin, callback) {
@@ -26,6 +26,8 @@ app.use(express.json());
 
 app.post('/api/chat', async (req, res) => {
     const { prompt } = req.body;
+    console.log('Received prompt:');
+    console.log(prompt);
     try {
         const response = await fetch(ollamaUrl + "/api/generate", {
             method: 'POST',
@@ -42,6 +44,8 @@ app.post('/api/chat', async (req, res) => {
         try {
             const data = JSON.parse(rawText);
             res.json({ reply: data.response || 'Sin respuesta procesable.' });
+            console.log('Response from Ollama:')
+            console.log(data); 
         } catch (parseError) {
             console.error('Error al parsear JSON:', parseError);
             res.status(500).json({ error: 'Respuesta inválida del modelo', raw: rawText });
